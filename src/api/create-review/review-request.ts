1345de1b243a0875cd864aa75dad4cfa6fe48e5d
Célia Doolaeghe <celia.doolaeghe@ext.adeo.com>
2021-11-09 17:06:27 +0100

    docs: add swagger description

--- src/api/create-review/review-request.ts
+++ src/api/create-review/review-request.ts
@@ -2,17 +2,17 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class ReviewRequest {
-  @ApiProperty()
+  @ApiProperty({ description: 'Title of the series' })
   @IsNotEmpty()
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Grade between 0 and 5' })
   @IsNumber()
   @Min(0)
   @Max(5)
   grade: number;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({ description: 'A comment on the series' })
   @IsOptional()
   @IsNotEmpty()
   comment?: string;
